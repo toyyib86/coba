74a2e3daa9fca21bba277f33e78712865880042a
toyyib86 <70227727+toyyib86@users.noreply.github.com>
2020-09-02 02:24:37 +0700

    Add files via upload

---
+++ list.sh
@@ -0,0 +1,76 @@
+python nuyul.py +16786316424
+sleep 0
+python nuyul.py +16786494658
+sleep 0
+python nuyul.py +16784332601
+sleep 0
+python nuyul.py +16787236399
+sleep 0
+python nuyul.py +15672459912
+sleep 0
+python nuyul.py +13159840306
+sleep 0
+python nuyul.py +13155030870
+sleep 0
+python nuyul.py +13159337909
+sleep 0
+python nuyul.py +15672819782
+sleep 0
+python nuyul.py +13152910674
+sleep 0
+python nuyul.py +13159230026
+sleep 0
+python nuyul.py +13158923972
+sleep 0
+python nuyul.py +16802143936
+sleep 0
+python nuyul.py +16804350315
+sleep 0
+python nuyul.py +16806979411
+sleep 0
+python nuyul.py +13159095899
+sleep 0
+python nuyul.py +13159871454
+sleep 0
+python nuyul.py +13154178037
+sleep 0
+python nuyul.py +15672000308
+sleep 0
+python nuyul.py +15672000545
+sleep 0
+python nuyul.py +15672008219
+sleep 0
+python nuyul.py +13159960815
+sleep 0
+python nuyul.py +13159361114
+sleep 0
+python nuyul.py +13153045592
+sleep 0
+python nuyul.py +13159041402
+sleep 0
+python nuyul.py +13156844015
+sleep 0
+python nuyul.py +13155440185
+sleep 0
+python nuyul.py +15672438044
+sleep 0
+python nuyul.py +15672438093
+sleep 0
+python nuyul.py +15672978068
+sleep 0
+python nuyul.py +13159746876
+sleep 0
+python nuyul.py +16785051544
+sleep 0
+python nuyul.py +16788102461
+sleep 0
+python nuyul.py +16802002299
+
+
+
+
+
+
+
+
+
